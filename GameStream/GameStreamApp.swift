51ff7905ab7bc534667fae5d332b1f1a4e40b6ef
Anthony RUbio <rubionn27@gmail.com>
2024-07-08 19:34:55 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Anthony Rubio on 8/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
